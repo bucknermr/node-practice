f9f3514e9bb523a94dad5f60d9fcc36aba66b2a6
Matthew Buckner <bucknermr@gmail.com>
2018-01-22 14:56:19 -0800

    Add emitter pt1 exercise

---
+++ E3_EventEmitterPart1/Starter/app.js
@@ -0,0 +1,14 @@
+const Emitter = require('./emitter');
+
+const emtr = new Emitter();
+
+emtr.on('greet', () => {
+    console.log('Somewhere, someone said hello.');
+});
+
+emtr.on('greet', () => {
+    console.log('A greeting occurred!');
+});
+
+console.log('Hello!');
+emtr.emit('greet');
