2f64775c6faecc8ac4b5ec40b34328576baa1adc
Matthew Buckner <bucknermr@gmail.com>
2018-01-23 14:15:30 -0800

    Add bodyParser middleware and routes for posting json or urlencoded data

--- I1_InstallingExpressWebServer/Starter/app.js
+++ I1_InstallingExpressWebServer/Starter/app.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
 
 const port = process.env.PORT || 3000;
+
+const urlencodedParser = bodyParser.urlencoded({ extended: false });
+const jsonParser = bodyParser.json();
 
 app.use('/', (req, res, next) => {
   console.log(`Request Url: ${req.url}`);
@@ -17,7 +21,17 @@ app.get('/', (req, res) => {
 });
 
 app.get('/person/:id', (req, res) => {
-  res.render('person', { ID: req.params.id });
+  res.render('person', { ID: req.params.id, name: req.query.name });
+});
+
+app.post('/person', urlencodedParser, (req, res) => {
+  console.log(req.body.firstName, req.body.lastName);
+  res.send('Thank you!');
+});
+
+app.post('/personjson', jsonParser, (req, res) => {
+  console.log(req.body.firstName, req.body.lastName);
+  res.send('Thank you for the JSON data!');
 });
 
 app.get('/api', (req, res) => {
