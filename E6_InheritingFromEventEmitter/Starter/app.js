d79e0fa1c2c63cc1721569472605806a461f1e06
Matthew Buckner <bucknermr@gmail.com>
2018-01-22 16:01:12 -0800

    Convert inheritance from EventEmitter exercise to ES6 class syntax

--- E6_InheritingFromEventEmitter/Starter/app.js
+++ E6_InheritingFromEventEmitter/Starter/app.js
@@ -1,22 +1,41 @@
 var EventEmitter = require('events');
-var util = require('util');
+// var util = require('util');
+//
+// function Greetr() {
+//     EventEmitter.call(this);
+//     this.greeting = "Hello world!";
+// }
+//
+// util.inherits(Greetr, EventEmitter);
+//
+// Greetr.prototype.greet = function(data) {
+//     console.log(this.greeting + ': ' + data);
+//     this.emit('greet', data);
+// };
+//
+// var greeter1 = new Greetr();
+//
+// greeter1.on('greet', (data) => {
+//     console.log('Someone greeted!: ' + data);
+// });
+//
+// greeter1.greet('Matt');
 
-function Greetr() {
-    EventEmitter.call(this);
+class Greetr extends EventEmitter {
+  constructor() {
+    super();
     this.greeting = "Hello world!";
+  }
+
+  greet(data) {
+    console.log(`${this.greeting}: ${data}`);
+    this.emit('greet', data);
+  }
 }
 
-util.inherits(Greetr, EventEmitter);
-
-Greetr.prototype.greet = function(data) {
-    console.log(this.greeting + ': ' + data);
-    this.emit('greet', data); 
-};
-
-var greeter1 = new Greetr();
-
-greeter1.on('greet', (data) => {
-    console.log('Someone greeted!: ' + data);
+const greeter2 = new Greetr();
+greeter2.on('greet', data => {
+  console.log(`Somone greeted: ${data}`);
 });
 
-greeter1.greet('Matt');
+greeter2.greet("Matthew");
