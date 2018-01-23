74207c866c681e5a1b5c66f423326dd6324325b3
Matthew Buckner <bucknermr@gmail.com>
2018-01-23 10:37:21 -0800

    Add outputting json exercise

---
+++ G9_OutputtingJSON/Starter/app.js
@@ -0,0 +1,10 @@
+const http = require('http');
+
+http.createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'application/json' });
+  const obj = {
+    firstName: 'John',
+    lastName: 'Doe'
+  };
+  res.end(JSON.stringify(obj));
+}).listen(1337, '127.0.0.1');
