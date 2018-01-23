a87dc4d22afc6bcc26247c766cfee233eefdb28a
Matthew Buckner <bucknermr@gmail.com>
2018-01-23 01:22:31 -0800

    Add read stream to fetch index.html asynchronously

--- G6_OutputtingHTMLAndTemplates/Starter/app.js
+++ G6_OutputtingHTMLAndTemplates/Starter/app.js
@@ -3,6 +3,9 @@ const fs = require('fs');
 
 http.createServer((req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/html' });
-  const html = fs.readFileSync(__dirname + '/index.html');
-  res.end(html);
+
+  // const html = fs.readFileSync(__dirname + '/index.html');
+  // res.end(html);
+
+  fs.createReadStream(__dirname + '/index.html').pipe(res);
 }).listen(1337, '127.0.0.1');
