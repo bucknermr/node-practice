432110f8adea5db5ddefcfb38ee22318d9c3e257
Matthew Buckner <bucknermr@gmail.com>
2018-01-23 13:30:28 -0800

    Add static assets and middleware request logger

--- I1_InstallingExpressWebServer/Starter/app.js
+++ I1_InstallingExpressWebServer/Starter/app.js
@@ -3,8 +3,15 @@ const app = express();
 
 const port = process.env.PORT || 3000;
 
+app.use('/', (req, res, next) => {
+  console.log(`Request Url: ${req.url}`);
+  next();
+});
+
+app.use('/assets', express.static(__dirname + '/public'));
+
 app.get('/', (req, res) => {
-  res.send('<html><body><h1>Hello World!</h1></body></html>');
+  res.send('<html> <head><link href="assets/style.css" type="text/css" rel="stylesheet" /></head> <body><h1>Hello World!</h1></body> </html>');
 });
 
 app.get('/person/:id', (req, res) => {
